2abcfaf5f69fd20ee7ac2602912c32882bedfa41
bilal-23 <mansuribilal101@gmail.com>
2021-06-26 19:17:12 +0530

    setting up files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCiEpt2tpdUMwscKW6vVRhR7FSYX-CFRqg",
+    authDomain: "netflixclone-23.firebaseapp.com",
+    projectId: "netflixclone-23",
+    storageBucket: "netflixclone-23.appspot.com",
+    messagingSenderId: "67601738441",
+    appId: "1:67601738441:web:af0f538c2ad127637a7b18"
+};
