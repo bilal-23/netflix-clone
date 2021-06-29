7d80c28b3b500faae6c9a10cc6c097e4762ea5f3
bilal-23 <mansuribilal101@gmail.com>
2021-06-29 13:27:00 +0530

    fixed name issue in cards

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './MovieCard.scss';
 
 const MovieCard = ({ movie }) => {
+
     const image_url = `https://image.tmdb.org/t/p/original/`;
     console.log(movie);
     return (
@@ -9,7 +10,7 @@ const MovieCard = ({ movie }) => {
             <div className="movie__card__overlay"></div>
             <img className={`movie__poster`} src={`${image_url}${movie?.poster_path}`} alt={movie?.title} />
             <div className="movie__card__info">
-                <h3 className="movie__card__name">{movie?.name}</h3>
+                <h3 className="movie__card__name">{movie?.name || movie?.title || movie?.original_title}</h3>
                 <div className="movie__card__additionalInfo">
                     <span className="movie__card__rating">{movie?.vote_average * 10}% Match</span> <span className="movie__card__year">{movie?.first_air_date?.substr(0, 4)}</span>
                 </div>
