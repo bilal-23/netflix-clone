77a2e3e7fe96c6b94a3c50675a05c88ea2255fc9
bilal-23 <mansuribilal101@gmail.com>
2021-07-01 15:59:32 +0530

    refactored requets

--- src/App.js
+++ src/App.js
@@ -1,76 +1,23 @@
 import React, { useEffect, useState, Suspense } from 'react';
-import { useDispatch } from 'react-redux';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import requests from './axios/Request';
-import { moviesActions } from './store/moviesSlice';
-import useFetchMovies from './hooks/use-fetchMovies';
+import useMovies from './hooks/use-Movies';
 import Homescreen from './components/Homescreen';
 import NetflixIntro from './components/UI/NetflixIntro';
 import Nav from './components/UI/Nav';
-// import ShowDetails from './components/ShowDetails';
-import './App.scss';
 import Loading from './components/UI/Loading';
-
-
-// const Homescreen = React.lazy(() => import("./components/Homescreen"));
+import './App.scss';
 const ShowDetails = React.lazy(() => import("./components/ShowDetails"));
 
-
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [showIntro, setShowIntro] = useState(true);
+  const fetchAllMovies = useMovies();
 
-  const [showIntro, setShowIntro] = useState(false);
-  const dispatch = useDispatch();
-  const fetchMovie = useFetchMovies();
   useEffect(() => {
-    const movie = async (fetchUrl) => {
-      const data = await fetchMovie(fetchUrl);
-      return data.results;
-    }
-    const fetchAllMovies = async () => {
-      const netflixOriginals = await movie(requests.fetchNetflixOriginals);
-      dispatch(moviesActions.updateNetflixOriginals(netflixOriginals));
+    setIsLoading(true);
+    fetchAllMovies(setIsLoading);
+  }, [fetchAllMovies])
 
-      const trending = await movie(requests.fetchTrending);
-      dispatch(moviesActions.updateTrending(trending));
-
-      const topRated = await movie(requests.fetchTopRated);
-      dispatch(moviesActions.updateTopRated(topRated));
-
-      const action = await movie(requests.fetchActionMovies);
-      dispatch(moviesActions.updateAction(action));
-
-      const comedy = await movie(requests.fetchComedyMovies);
-      dispatch(moviesActions.updateComedy(comedy));
-
-      const horror = await movie(requests.fetchHorrorMovies);
-      dispatch(moviesActions.updateHorror(horror));
-
-      const romance = await movie(requests.fetchRomanceMovies);
-      dispatch(moviesActions.updateRomance(romance));
-
-      const documentaries = await movie(requests.fetchDocumentaries);
-      dispatch(moviesActions.updateDocumentaries(documentaries));
-
-      const allMovies = {
-        netflixOriginals: [...netflixOriginals],
-        trending: [...trending],
-        topRated: [...topRated],
-        action: [...action],
-        comedy: [...comedy],
-        horror: [...horror],
-        romance: [...romance],
-        documentaries: [...documentaries],
-        expireTime: Date.now() + 3600000
-      }
-      localStorage.setItem("allMovies", JSON.stringify(allMovies));
-    }
-    if (!localStorage.getItem("allMovies")) {
-      fetchAllMovies();
-    } else if (JSON.parse(localStorage.getItem("allMovies")).expireTime < Date.now()) {
-      localStorage.removeItem("allMovies");
-    }
-
-  }, [dispatch, fetchMovie])
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -79,6 +26,10 @@ function App() {
 
     return (() => clearTimeout(timer));
   })
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <div className="App">
--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -1,28 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import Banner from './Banner';
 import Row from './Row';
-import Loading from './UI/Loading';
 import './Homescreen.scss';
 
 const Homescreen = () => {
-    const [isLoading, setIsLoading] = useState(true);
     const movies = useSelector(state => state.movies);
-    const bannerMovie = movies.netflixOriginals[Math.floor(Math.random() * movies.netflixOriginals.length - 1)]
+    const bannerMovie = movies.netflixOriginals[Math.floor(Math.random() * movies.netflixOriginals.length - 1)];
+
     return (
         <div className="homescreen">
 
             <Banner bannerMovie={bannerMovie} />
-            <div className="rows">
+            {movies && <div className="rows">
                 <Row title="Netflix Originals" mediaType="tv" movies={movies.netflixOriginals} />
-                <Row title="trending now" mediaType="" movies={movies.trending} />
-                <Row title="top rated" mediaType="movie" movies={movies.topRated} />
-                <Row title="action movies" mediaType="movie" movies={movies.action} />
-                <Row title="comedy movies" mediaType="movie" movies={movies.comedy} />
-                <Row title="horror movies" mediaType="movie" movies={movies.horror} />
-                <Row title="romance movies" mediaType="movie" movies={movies.romance} />
-                <Row title="documentaries" mediaType="movie" movies={movies.documentaries} />
-            </div>
+                <Row title="trending now" mediaType=""
+                    movies={movies.trending} />
+                <Row title="top rated" mediaType="movie"
+                    movies={movies.topRated} />
+                <Row title="action movies" mediaType="movie"
+                    movies={movies.action} />
+                <Row title="comedy movies" mediaType="movie"
+                    movies={movies.comedy} />
+                <Row title="horror movies" mediaType="movie"
+                    movies={movies.horror} />
+                <Row title="romance movies" mediaType="movie"
+                    movies={movies.romance} />
+                <Row title="documentaries" mediaType="movie"
+                    movies={movies.documentaries} />
+            </div>}
         </div>
     )
 }
